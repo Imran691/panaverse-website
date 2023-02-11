0da0fa2253b1d7fe46e8eebbba56288aedb5a7c5
Imran691 <mimran691@gmail.com>
2023-02-11 20:47:47 +0500

    home page made responsive

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,37 +1,48 @@
-import {
-  Box,
-  ChakraBaseProvider,
-  Heading,
-  Text,
-} from "@chakra-ui/react";
+import { Box, ChakraBaseProvider, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 export default function Home() {
   return (
     <ChakraBaseProvider>
-      <Box p={"150px"}>
+      <Box p={{base:"50px", sm:"75px", lg:"100px"}}>
         <Box pt={"45px"}>
-          <Heading textAlign={"center"} fontSize={"50px"} fontWeight={"bold"}>
+          <Heading
+            textAlign={{ base: "left", sm: "left", lg: "center" }}
+            fontSize={{ base: "10px", sm: "20px", lg: "50px" }}
+            fontWeight={"bold"}
+          >
             Certified Web 3.0 and Metaverse Developer: A Nationwide Program in
             Karachi, Lahore, Islamabad, and Peshawar
           </Heading>
         </Box>
-        <Box pt={"45px"}>
-          <Text textAlign={"center"} fontSize={"30px"} fontWeight="bold">
+        <Box pt={{base:"20px", sm:"30px", lg:"45px"}}>
+          <Text
+            textAlign={{ base: "left", sm: "left", lg: "center" }}
+            fontSize={{ base: "7px", sm: "15px", lg: "30px" }}
+            fontWeight="bold"
+          >
             Getting Ready for the Next Generation and Future of the Internet -
             Join a 13 Trillion Dollar Industry with 5 Billion Users
           </Text>
         </Box>
-        <Box pt={"45px"}>
-          <Text textAlign={"center"} fontSize="20px" fontWeight={"semibold"}>
+        <Box pt={{base:"20px", sm:"30px", lg:"45px"}}>
+          <Text
+            textAlign={{ base: "left", sm: "left", lg: "center" }}
+            fontSize={{ base: "5px", sm: "10px", lg: "20px" }}
+            fontWeight={"semibold"}
+          >
             The Future of the Web is Web 3.0, Metaverse, and Edge Computing.
             Panaverse DAO is a movement to spread these technolgies globally. It
             is community of Web 3 and Metaverse developers, designers, trainers,
             startup founders and service providers.
           </Text>
         </Box>
-        <Box pt={"85px"}>
-          <Text textAlign={"justify"} fontSize="20px" fontWeight={"semibold"}>
+        <Box pt={{base:"20px", sm:"30px", lg:"45px"}}>
+          <Text
+            textAlign={{ base: "left", sm: "left", lg: "center" }}
+            fontSize={{ base: "5px", sm: "10px", lg: "20px" }}
+            fontWeight={"semibold"}
+          >
             The internet is without a doubt the most important technological
             development in human history. Web3 and metaverse technologies expand
             the internet as we know it by introducing novel features and
