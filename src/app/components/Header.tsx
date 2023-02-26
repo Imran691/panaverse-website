7ca3780ba2abf1ac2e8485d4a639a01ccaf2390d
Imran691 <mimran691@gmail.com>
2023-02-26 09:47:50 +0500

    set right responsiveness of header for base devices

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -21,35 +21,39 @@ import {
 import { HamburgerIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import Image from "next/image";
-import Logo from"../../../public/Logo.png"
-
+import Logo from "../../../public/Logo.png";
 
 export default function Header() {
   return (
-      <Box boxShadow={"lg"}>
-        <Container maxWidth={1400}>
-          <SimpleGrid
-            templateColumns={{ sm: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+    <Box boxShadow={"lg"}>
+      <Container maxWidth={1400}>
+        <SimpleGrid
+          templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+        >
+          <Box>
+            <Image
+              src={Logo}
+              alt="panaverse-logo"
+              height={150}
+              width={150}
+            ></Image>
+          </Box>
+          <Flex
+            color={"black"}
+            fontSize={{ base: "15px", lg: "20px" }}
+            fontWeight="semibold"
+            gap={10}
+            placeItems={"center"}
+            display={{ base: "none", lg: "flex" }}
           >
-            <Box>
-              <Image src={Logo} alt="panaverse-logo" height={150} width={150}></Image>
-            </Box>
-            <Flex
-              color={"black"}
-              fontSize={{ base: "15px", lg: "20px" }}
-              fontWeight="semibold"
-              gap={10}
-              placeItems={"center"}
-              display={{ base: "none", lg: "flex" }}
-            >
-              <Link href={"/"}>Home</Link>
-              <Link href={"/about"}>About</Link>
-              <Link href={"/syllabus"}>Syllabus</Link>
-              <Link href={"/explore"}>Explore</Link>
-              <Link href={"/contact"}>Contact</Link>
-            </Flex>
-            <Box pt={9} display={{ base: "none", lg: "initial" }}>
-              <Link href={"https://portal.piaic.org/"}>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/about"}>About</Link>
+            <Link href={"/syllabus"}>Syllabus</Link>
+            <Link href={"/explore"}>Explore</Link>
+            <Link href={"/contact"}>Contact</Link>
+          </Flex>
+          <Box pt={9} display={{ base: "none", lg: "initial" }}>
+            <Link href={"https://portal.piaic.org/"}>
               <Button
                 size={"md"}
                 bg={"#e9b3e9"}
@@ -60,29 +64,39 @@ export default function Header() {
               >
                 Login/Signup
               </Button>
-              </Link>
-            </Box>
+            </Link>
+          </Box>
 
-            <Box display={{ base: "initial", lg: "none" }} pt="15px">
-              <Menu>
-                <MenuButton
-                  float={"right"}
-                  as={IconButton}
-                  aria-label="Options"
-                  icon={<HamburgerIcon />}
-                  variant="outline"
-                />
-                <MenuList>
-                  <MenuItem><Link href={"/"}>Home</Link></MenuItem>
-                  <MenuItem><Link href={"/about"}>About</Link></MenuItem>
-                  <MenuItem><Link href={"/syllabus"}>Syllabus</Link></MenuItem>
-                  <MenuItem><Link href={"/explore"}>Explore</Link></MenuItem>
-                  <MenuItem><Link href={"/contact"}>Contact us</Link></MenuItem>
-                </MenuList>
-              </Menu>
-            </Box>
-          </SimpleGrid>
-        </Container>
-      </Box>
+          <Box display={{ base: "initial", lg: "none" }} pt="15px">
+            <Menu>
+              <MenuButton
+                float={"right"}
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+              />
+              <MenuList>
+                <MenuItem>
+                  <Link href={"/"}>Home</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href={"/about"}>About</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href={"/syllabus"}>Syllabus</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href={"/explore"}>Explore</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href={"/contact"}>Contact us</Link>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 }
